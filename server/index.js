7a9040a07a9ea4005bd18ec9b5e66f7dc4ad0ac7
CezaryB <czarekburzykowski@gmail.com>
2017-11-30 16:49:57 +0100

    seperate router file

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const http = require('http')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
+const router = require('./router')
 
 // App Setup
 
-app.get('/', (req, res, next) => {
-  res.send('Hello')
-})
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
+router(app)
 
 // Server Setup
 
