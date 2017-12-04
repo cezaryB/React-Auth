c1de18fe58f322c3c5eeea1d9da6615d5cc06753
Cezary <czarekburzykowski@gmail.com>
2017-12-04 21:30:55 +0100

    next commit

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,3 +3,29 @@ const User = require('../models/user')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+
+// Setup options for JWT Strategy
+
+const jwtOptions = {}
+
+// Create JWT Strategy
+
+    // payload is decoded jwt token, czyli będzie miał sub property i iat property bez tokena
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if the user ID in the payload exist in our database
+  User.findById(payload.sub, function(err, user) {
+    if (err) {
+      done(err, false)
+    }
+    // If it does, call 'done' with that user
+    if (user) {
+      done(null, user)
+    }
+    else {
+      done(null, false)
+    }
+    // Otherwise, call done without a user object
+  })
+})
+
+// Tell passport to use this strategy
